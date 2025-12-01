76d7903a8bd163a8c5039ee44ed6f7c72cd3e88f
lmkptr <dinqfh@gmail.com>
2025-12-01 10:47:33 +0100

    "new functions"

--- python/main.py
+++ python/main.py
@@ -1,7 +1,10 @@
 print("Hello, World!")
-
-def greet(name):
-    return f"Hello, {name}!"
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    print(greet(user_name))
+dict = {"min": 1, "max": 5}
+list_of_numbers = [1, 2, 3, 4, 5]
+print ("List of numbers:", list_of_numbers[1])
+print(dict["min"], dict["max"])
+total = sum(list_of_numbers)
+print("Total:", total)
+name = 'Dima'
+print(dir(name))
+print(name.translate({ord('D'): 'd'}))
